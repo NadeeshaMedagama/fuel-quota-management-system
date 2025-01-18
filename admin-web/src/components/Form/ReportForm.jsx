c6ec3656e8229740c55773087d9e303b7b19388e
Raveesha-Amarawickrama <amarawi-se21004@stu.kln.ac.lk>
2025-01-18 16:16:09 +0530

    modify Frm.css,ReportForm.css and ReportForm.jsx

--- admin-web/src/components/Form/ReportForm.jsx
+++ admin-web/src/components/Form/ReportForm.jsx
@@ -1,35 +1,49 @@
-import React, { useState } from 'react';
-import './ReportForm.css';
+import React, { useState } from "react";
+import "./ReportForm.css";
 
 function ReportForm({ generateReport }) {
-  const [reportType, setReportType] = useState('daily');
-  const [fuelType, setFuelType] = useState('petrol');
+  const [reportType, setReportType] = useState("daily");
+  const [fuelType, setFuelType] = useState("petrol");
 
   const handleGenerateClick = () => {
     generateReport(fuelType, reportType);
   };
 
   return (
-    <div>
-      <h2>Select Report Type</h2>
-      <form>
-        <label>
-          Fuel Type:
-          <select value={fuelType} onChange={(e) => setFuelType(e.target.value)}>
-            <option value="petrol">Petrol</option>
-            <option value="diesel">Diesel</option>
-            <option value="gas">Gas</option>
-          </select>
-        </label>
-        <label>
-          Report Period:
-          <select value={reportType} onChange={(e) => setReportType(e.target.value)}>
-            <option value="daily">Daily</option>
-            <option value="weekly">Weekly</option>
-            <option value="monthly">Monthly</option>
-          </select>
-        </label>
-        <button type="button" onClick={handleGenerateClick}>
+    <div className="report-form-wrapper">
+      <h2 className="form-header">Select Report Type</h2>
+      <form className="report-form">
+        <div className="form-group">
+          <label>
+            Fuel Type:
+            <select
+              value={fuelType}
+              onChange={(e) => setFuelType(e.target.value)}
+            >
+              <option value="petrol">Petrol</option>
+              <option value="diesel">Diesel</option>
+              <option value="gas">Gas</option>
+            </select>
+          </label>
+        </div>
+        <div className="form-group">
+          <label>
+            Report Period:
+            <select
+              value={reportType}
+              onChange={(e) => setReportType(e.target.value)}
+            >
+              <option value="daily">Daily</option>
+              <option value="weekly">Weekly</option>
+              <option value="monthly">Monthly</option>
+            </select>
+          </label>
+        </div>
+        <button
+          type="button"
+          className="generate-report-button"
+          onClick={handleGenerateClick}
+        >
           Generate Report
         </button>
       </form>
