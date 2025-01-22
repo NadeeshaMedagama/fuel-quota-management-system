ee3f09eda75694b5e63b5e50b22322dfdcb96a90
Raveesha-Amarawickrama <amarawi-se21004@stu.kln.ac.lk>
2025-01-22 22:03:02 +0530

    Update Sidebar.jsx
    Change structure of the code only

--- admin-web/src/components/Dashboard/Sidebar.jsx
+++ admin-web/src/components/Dashboard/Sidebar.jsx
@@ -6,49 +6,47 @@ import "./Sidebar.css";
 
 const Sider = () => {
   return (
-    
-    <Navbar className="sider-container web-sider" expand="lg" variant="dark">
-      {/* Logo Section */}
-      
-      <div className="logo-container">
-        <img src={logo} alt="Logo" className="sidebar-logo" />
-      </div>
+    <Navbar
+      className="sider-container web-sider d-flex flex-column vh-100"
+      expand="lg">
 
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+     
+      <Navbar.Brand className="text-center py-3">
+        <img src={logo} alt="Logo" className="sidebar-logo" />
+      </Navbar.Brand>
+
+
+
+      <Navbar.Toggle aria-controls="basic-navbar-nav" className="mb-3 mx-auto" />
       <Navbar.Collapse id="basic-navbar-nav">
-        <div className="nav_block">
-          <Nav className="flex-column sider-menu">
-            <Nav.Item>
-              <Link to="/vehicles" className="nav-link">
-                Manage Vehicles
-              </Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Link to="/fuel-stations" className="nav-link">
-                Manage Fuel Station
-              </Link>
-            </Nav.Item>
-           
-            <Nav.Item>
-              <Link to="/distribution-tracking" className="nav-link">
-                Distribution
-              </Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Link to="/fuel-quota" className="nav-link">
-                Quota Management
-              </Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Link to="/reports" className="nav-link">
-                Generate Reports
-              </Link>
-            </Nav.Item>
-          </Nav>
-        </div>
+        <Nav className="flex-column sider-menu">
+
+          <Nav.Item>
+            <Link to="/vehicles" className="nav-link"> Manage Vehicles</Link>
+          </Nav.Item>
+
+          <Nav.Item>
+            <Link to="/fuel-stations" className="nav-link"> Manage Fuel Station </Link>
+          </Nav.Item>
+
+          <Nav.Item>
+            <Link to="/distribution-tracking" className="nav-link">Distribution</Link>
+          </Nav.Item>
+
+
+          <Nav.Item>
+            <Link to="/fuel-quota" className="nav-link">Quota Management</Link>
+          </Nav.Item>
+
+
+          <Nav.Item>
+            <Link to="/reports" className="nav-link">Generate Reports </Link>
+          </Nav.Item>
+
+        </Nav>
+        
       </Navbar.Collapse>
     </Navbar>
-    
   );
 };
 
