1a102cc7c4769ecddea419652f00ff450d1f6391
Nadeesha Medagama <nadeeshamedagama@gmail.com>
2025-01-27 14:25:55 +0530

    twillio service updated

--- Fuel Backend/src/main/java/com/FuelBackend/controller/NotificationController.java
+++ Fuel Backend/src/main/java/com/FuelBackend/controller/NotificationController.java
@@ -3,9 +3,11 @@ package com.FuelBackend.controller;
 import com.FuelBackend.service.notificationServices.TwilioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/notifications")
 public class NotificationController {
 
     private final TwilioService twilioService;
@@ -17,10 +19,10 @@ public class NotificationController {
 
     @GetMapping("/send-notification")
     public String sendNotification() {
-        String toPhoneNumber = "0716889714";
+        String toPhoneNumber = "+94726406224";
         String message = "Your fuel quota has been updated.";
         twilioService.sendSms(toPhoneNumber, message);
-        return "Notification sent!";
+        return "SMS sent successfully!";
     }
 
 
